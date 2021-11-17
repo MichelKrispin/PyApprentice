cea2913a1fb52ebebb77d14016fab23eab5c17b4
Michel Krispin <michel.krispin@gmail.com>
2021-11-17 12:59:57 +0100

    Updates to exception handling

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 import yaml
 import json
-import re
+
 import traceback
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
 
+from shutil import copyfile
 from io import StringIO
 from contextlib import redirect_stdout
 
 
 class CellHandler:
     def __init__(self):
-        with open('demo.yaml', 'r') as f:
+        self.file_name = 'demo.yaml'
+        with open(self.file_name, 'r') as f:
             self.data = yaml.safe_load(f)
 
     def send_as_str(self):
         return json.dumps(self.data)
 
-    def check(self, code, id):
+    def save(self):
+        """Save the data to file."""
+        copyfile(self.file_name, self.file_name + '.backup')
+        with open(self.file_name, 'w') as f:
+            yaml.dump(self.data, f, default_flow_style=False)
+
+    def check(self, global_code, code, id):
         """Runs the code sandboxed and
         calls the check function with the result.
 
@@ -27,45 +35,84 @@ class CellHandler:
             code ([type]): [description]
             id ([type]): [description]
         """
-        scope = {'__name__': '__main__',
-                 '__builtins__': globals()['__builtins__']}
+        # Get the correct cell or raise an exception if it doesn't exist
+        cell = None
+        for c in self.data['cells']:
+            if c['id'] == id:
+                cell = c
+                break
+        if not cell:
+            raise IndexError(f'Id {id} not found in the cells')
 
+        # Run the global code before the users code is run
+        scope = {'__name__': 'global_code', '__builtins__': globals()['__builtins__']}
+        caught_exception = False
+        result = False
         output = ''
-        f = StringIO()
-        with redirect_stdout(f):
-            try:
-                exec(code, scope)
-            except Exception as e:
-                output = traceback.format_exc()
-                # Remove the traceback for this file
-                # TODO: Fix regex
-                output = re.sub(
-                    r'  File.+[\r\n]+.+(?=exec\(code, scope\))?',
-                    '', output)
-        output = f.getvalue() if not output else output
-        print(output)
-        # Find the checking function and run it in own scope
-        check_code = ''
-        for cell in self.data['cells']:
-            if cell['id'] == id:
-                check_code = cell['check']
-        check_code += '\nresult = check(_Scope, _Output)'
+        if global_code != '':
+            f = StringIO()
+            with redirect_stdout(f):
+                try:
+                    exec(global_code, scope)
+                except Exception as e:
+                    exception = traceback.format_exc()
+                    # Remove the traceback for this file
+                    lines = exception.split('\n')
+                    exercise_name = cell['title']
+                    output = '\n'.join([lines[0]] + lines[3:]) \
+                        .replace('<astring>', 'Global Code')
+            caught_exception = output != ''
+            output = output if caught_exception else f.getvalue()
 
-        check_scope = {'__name__': '__main__',
-                       '__builtins__': globals()['__builtins__'],
-                       '_Scope': scope, '_Output': output}
+            # If everything went right do a little bit of reflection magic
+            # such that every function created in the global functions scope
+            # knows that.
+            for attr in scope:
+                try:
+                    if scope[attr].__code__.co_filename == '<string>':
+                        scope[attr].__code__ = scope[attr].__code__.replace(co_filename='global_functions.py')
+                except AttributeError:
+                    pass
+        # Run the code sent by the user in the same scope
+        if not caught_exception:
+            scope['__name__'] = '__main__'
+            f = StringIO()
+            with redirect_stdout(f):
+                try:
+                    exec(code, scope)
+                except Exception as e:
+                    exception = traceback.format_exc()
+                    # Remove the traceback for this file
+                    lines = exception.split('\n')
+                    exercise_name = cell['title']
+                    output = '\n'.join([lines[0]] + lines[3:]) \
+                        .replace('<astring>', exercise_name)
+            caught_exception = output != ''
+            output = output if caught_exception else f.getvalue()
 
-        f = StringIO()
-        with redirect_stdout(f):
-            exec(check_code, check_scope)
-        print('------\n', f.getvalue(), '------\n')
-        result = check_scope['result']
+            # Find the checking function
+            check_code = cell['check']
+            check_code += '\nresult = check(_Scope, _Output)'
 
-        for i, cell in enumerate(self.data['cells']):
-            if cell['id'] == id:
-                self.data['cells'][i]['output'] = tornado.escape.xhtml_escape(
-                    output).replace('\n', '<br>').replace('    ', '&nbsp;&nbsp;&nbsp;&nbsp;')
-                self.data['cells'][i]['passed'] = result
+            check_scope = {'__name__': '__main__',
+                           '__builtins__': globals()['__builtins__'],
+                           '_Scope': scope, '_Output': output}
+
+            # Run the checking function in the created scope
+            f = StringIO()
+            with redirect_stdout(f):
+                exec(check_code, check_scope)
+            # print('------\n', f.getvalue(), '------\n')
+            result = check_scope['result']
+
+        # Save data
+        self.data['global-code'] = global_code
+        cell['code'] = code
+        cell['output'] = tornado.escape.xhtml_escape(
+            output).replace('\n', '<br>').replace('  ', '&nbsp;&nbsp;')
+        cell['passed'] = result
+        cell['response']['type'] = 'exception' if caught_exception else ('passed' if result else 'failed')
+        self.save()
         return output, result
 
 
@@ -81,8 +128,8 @@ class RunWebSocket(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         data = json.loads(message)
-        output, result = cell_handler.check(data['code'], data['id'])
-        print(f'f{output} which is {result}')
+        output, result = cell_handler.check(data['global-code'], data['code'], data['id'])
+        # print(f'{output}which is {result}\n')
         self.write_message(cell_handler.send_as_str())
 
     def on_close(self):
@@ -100,4 +147,5 @@ def make_app():
 if __name__ == "__main__":
     app = make_app()
     app.listen(8000)
+    print(f'Running PyApprentrice on http://localhost:8000/\n')
     tornado.ioloop.IOLoop.current().start()
--- static/main.js
+++ static/main.js
@@ -1,34 +1,46 @@
 /* Initialize the global editor. */
 const default_settings = {
-  showGutter: false,
-  showPrintMargin: false,
-  wrap: true,
-  theme: 'ace/theme/solarized_dark',
-  mode: 'ace/mode/python',
-  fontSize: '16pt',
+    showPrintMargin: false,
+    wrap: true,
+    theme: 'ace/theme/solarized_dark',
+    mode: 'ace/mode/python',
+    fontSize: '16pt',
 };
+
+// Make the gutter smaller
+const smallerGutterRenderer = {
+    getWidth: function (session, lastLineNumber, config) {
+        return lastLineNumber.toString().length * config.characterWidth - 12;
+    },
+    getText: function (session, row) {
+        return row + 1;
+    }
+};
+
 
 let editors = {
-  'global-editor': ace.edit('global-editor', default_settings),
+    'global-editor': ace.edit('global-editor', default_settings),
 };
+editors['global-editor'].session.gutterRenderer = smallerGutterRenderer
 
 function resizeEditor(id, numLines) {
-  document.getElementById(id).style.height =
-    numLines * editor.renderer.lineHeight + 'px';
-  editors[id].resize();
+    document.getElementById(id).style.height =
+        numLines * editor.renderer.lineHeight + 'px';
+    editors[id].resize();
 }
 
 /* Check for the WebSocket connection and act appropiately. */
 let ws = new WebSocket('ws://localhost:8000/ws');
 ws.onopen = function () {
-  // ws.send('Hello, world');
+    // ws.send('Hello, world');
 };
 ws.onmessage = function (evt) {
-  data = JSON.parse(evt.data);
-  document.getElementById('title').innerHTML = data['title'];
-  cells_html = Array.from(' '.repeat(data['cells'].length));
-  data['cells'].forEach((cell) => {
-    cells_html[cell['id']] = `
+    data = JSON.parse(evt.data);
+    editors['global-editor'].setValue(data['global-code']);
+    document.getElementById('title').innerHTML = data['title'];
+    cells_html = Array.from(' '.repeat(data['cells'].length));
+    data['cells'].forEach((cell) => {
+        cells_html[cell['id']] = `
         <div class="box">
           <div class="columns is-vcentered">
             <div class="column">
@@ -51,8 +63,8 @@ ws.onmessage = function (evt) {
           </div>
           <div class="editor" id="editor-${cell['id']}"></div>
       `;
-    if (cell['output']) {
-      cells_html[cell['id']] += `
+        if (cell['output']) {
+            cells_html[cell['id']] += `
             <article class="message">
               <div class="message-body" style="font-family: monospace">
                 <p>
@@ -61,24 +73,25 @@ ws.onmessage = function (evt) {
               </div>
             </article>
         `;
-    }
-    cells_html[cell['id']] += '</div>';
-  });
-  document.getElementById('tasks').innerHTML = cells_html.join('\n');
-  data['cells'].forEach((cell) => {
-    editor = ace.edit(`editor-${cell['id']}`, default_settings);
-    editor.setValue(cell['code']);
-    editors[`editor-${cell['id']}`] = editor;
-  });
-  initialized = true;
+        }
+        cells_html[cell['id']] += '</div>';
+    });
+    document.getElementById('tasks').innerHTML = cells_html.join('\n');
+    data['cells'].forEach((cell) => {
+        let editor = ace.edit(`editor-${cell['id']}`, default_settings);
+        editor.session.gutterRenderer = smallerGutterRenderer;
+        editor.setValue(cell['code']);
+        editors[`editor-${cell['id']}`] = editor;
+    });
 };
 
 function runCell(id) {
-  console.log();
-  ws.send(
-    JSON.stringify({
-      id: id,
-      code: editors[`editor-${id}`].getValue(),
-    })
-  );
+    console.log();
+    ws.send(
+        JSON.stringify({
+            id: id,
+            'global-code': editors['global-editor'].getValue(),
+            code: editors[`editor-${id}`].getValue(),
+        })
+    );
 }
